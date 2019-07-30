3ee89c3f0dce7fe5360721d296dab23890c1643e
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-07-30 16:06:35 +0300

    handleBinaryMesage

--- src/main/java/eos/websocket/api/SocketHandler.java
+++ src/main/java/eos/websocket/api/SocketHandler.java
@@ -1,14 +1,13 @@
 package eos.websocket.api;
 import com.google.gson.Gson;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.*;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -21,12 +20,14 @@ public class SocketHandler extends BinaryWebSocketHandler implements WebSocketHa
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
+        System.out.println(session.getAcceptedProtocol());
         System.out.println(session.getTextMessageSizeLimit());
     }
 
     @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) {
-        Map value = new Gson().fromJson(message.getPayload(), Map.class);
+    public void handleBinaryMessage(WebSocketSession session, BinaryMessage message){
+
+        System.out.println(message.getPayload().toString());
     }
 
     @Override
