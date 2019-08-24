72f0bac3365ff614a2e559a5c08c8412bb9d6c6d
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-08-24 18:53:57 +0300

    added python script and sample transaction for testing

--- src/main/java/eos/websocket/api/RedisMessagePublisher.java
+++ src/main/java/eos/websocket/api/RedisMessagePublisher.java
@@ -1,12 +1,14 @@
 package eos.websocket.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisMessagePublisher implements MessagePublisher{
+
 
     private final StringRedisTemplate stringRedisTemplate;
     private final ChannelTopic topic;
