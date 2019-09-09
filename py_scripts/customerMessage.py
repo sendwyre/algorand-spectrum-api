1d47d50a63443ad40ade6e13e0faaea893524418
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-09-09 11:14:02 +0300

    added example of customerMessage in py_scripts

--- py_scripts/customerMessage.py
+++ py_scripts/customerMessage.py
@@ -13,13 +13,11 @@ messageBody ={"account":"eostribe","actions":actionsList }
 # print(json.dumps(messageBody))
 #
 ws = create_connection(url)
-
 ws.ping()
 messageJson = json.dumps(messageBody)
 ws.send(messageJson)
 
 while True:
-    
     print(ws.recv())
 
 # actionsList = ["transfer","test1"]
