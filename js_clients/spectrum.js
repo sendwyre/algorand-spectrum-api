f947f397bf9536524c899cd05c224e2b2878ac38
Eugene Luzgin <eluzgin@Eugenes-MacBook-Pro-2.local>
2019-12-03 10:02:50 -0700

    Add unversal spectrum client JS library for any chain

---
+++ js_clients/spectrum.js
@@ -0,0 +1,87 @@
+// Spectrum JS client:
+var apikey = "test-api-key";
+
+// Call this method once top open connection:
+function open_connection(chain, apiKey, onOpenHandler, onMessageHandler, onCloseHandler, onErrorHandler) {
+	let endpoint = "wss://api."+chain+".spectrumeos.io/streaming"
+	if(apiKey != null) {
+		apikey = apiKey;
+	}
+	var socket = new WebSocket(endpoint);
+	socket.onopen = function(event) {
+		if(onOpenHandler != null) {
+			onOpenHandler(event);
+		} else {
+			console.log("On open handler not set, socket connection open!");
+		}
+	}
+	socket.onmessage = function(event) {
+		if(onMessageHandler != null) {
+			onMessageHandler(event);
+		} else {
+			console.log("On message handler not set, got message: "+event.data);
+		}
+	}
+	socket.onclose = function(event) {
+		if(onCloseHandler != null) {
+			onCloseHandler(event);
+		} else {
+			console.log("On close handler not set, socket connection closed!");
+		}
+	}
+	socket.onerror = function(error) {
+		if(onErrorHandler != null) {
+			onErrorHandler(error.message);
+		} else {
+			console.log("On error handler not set, got error: "+error.message);
+		}
+	}
+	return socket;
+}
+
+
+function get_blocks(socket) {
+	var messageBody = {
+   		"apikey": apikey,
+   		"event": "subscribe",
+   		"type": "get_blocks"
+	};
+  	socket.send(JSON.stringify(messageBody));
+}
+
+function get_transaction(socket, account) {
+	var messageBody = {
+   		"apikey": apikey,
+   		"event": "subscribe",
+   		"type": "get_transaction",
+        "data": {"account": account}
+	};
+  	socket.send(JSON.stringify(messageBody));
+}
+
+function get_actions(socket, account, actions) {
+	var messageBody = {
+   		"apikey": apikey,
+   		"event": "subscribe",
+   		"type": "get_actions",
+        "data": {"account": account, "actions": actions}
+	};
+  	socket.send(JSON.stringify(messageBody));
+}
+
+function get_table_rows(socket, account, table, scope) {
+	var messageBody = {
+   		"apikey": apikey,
+   		"event": "subscribe",
+   		"type": "get_table_rows",
+        "data": {
+    		"code": account,
+    		"table": table,
+    		"scope": scope
+   		}
+	};
+  	socket.send(JSON.stringify(messageBody));
+}
+
+
+
